7f611d378b3003a18e01cfa0d281613561e1bc06
doryan-meunier <doryan.meunier@gmail.com>
2026-03-25 12:24:23 +0100

    classe Student, Grade et Subject declarer

---
+++ main.py
@@ -0,0 +1,6 @@
+class Student:
+    def __init__(self, name, grade_math, grade_english, grade_science):
+        self.name = name
+        self.grade_math = grade_math
+        self.grade_english = grade_english
+        self.grade_science = grade_science

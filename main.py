c5175439c31554d2efb231c109809e1c653dc27f
doryan-meunier <doryan.meunier@gmail.com>
2026-03-25 12:37:36 +0100

    ajout rank_matter_1 method

--- main.py
+++ main.py
@@ -24,8 +24,14 @@ class SchoolClass:
     def add_student(self, student):
         self.students.append(student)
 
+    def rank_matter_1(self):
+        sorted_students = sorted(self.students, key=lambda s: s.grade_math, reverse=True)
+        for student in sorted_students:
+            print(student.name, student.grade_math)
 
 school_class = SchoolClass()
 school_class.add_student(Student('J', 10, 12, 13))
 school_class.add_student(Student('A', 8, 2, 17))
 school_class.add_student(Student('V', 9, 14, 14))
+
+school_class.rank_matter_1()

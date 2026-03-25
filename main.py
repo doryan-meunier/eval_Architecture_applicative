5bc697e18b0955d5a599d9b1c3d5280d91e54697
doryan-meunier <doryan.meunier@gmail.com>
2026-03-25 13:02:59 +0100

    ajout StudentWithHistory decorator and Iterator4

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from collections.abc import Iterable, Iterator
 
+
 class Subject:
     def __init__(self, name):
         self.name = name
@@ -18,6 +19,13 @@ class Student:
         self.grade_english = grade_english
         self.grade_science = grade_science
 
+
+class StudentWithHistory(Student):
+    def __init__(self, student, grade_history):
+        super().__init__(student.name, student.grade_math, student.grade_english, student.grade_science)
+        self.grade_history = grade_history
+
+
 class StudentIterator(Iterator):
     def __init__(self, students):
         self._students = sorted(students, key=lambda s: s.grade_math, reverse=True)
@@ -29,7 +37,8 @@ class StudentIterator(Iterator):
         student = self._students[self._index]
         self._index += 1
         return student
-    
+
+
 class StudentIterator2(Iterator):
     def __init__(self, students):
         self._students = sorted(students, key=lambda s: s.grade_english, reverse=True)
@@ -54,8 +63,22 @@ class StudentIterator3(Iterator):
         student = self._students[self._index]
         self._index += 1
         return student
-    
-class SchoolClass:
+
+
+class StudentIterator4(Iterator):
+    def __init__(self, students):
+        self._students = sorted(students, key=lambda s: s.grade_history, reverse=True)
+        self._index = 0
+
+    def __next__(self):
+        if self._index >= len(self._students):
+            raise StopIteration
+        student = self._students[self._index]
+        self._index += 1
+        return student
+
+
+class SchoolClass(Iterable):
     def __init__(self):
         self.students = []
 
@@ -64,12 +87,12 @@ class SchoolClass:
 
     def __iter__(self):
         return StudentIterator(self.students)
-    
+
     def rank_matter_1(self):
         sorted_students = sorted(self.students, key=lambda s: s.grade_math, reverse=True)
         for student in sorted_students:
             print(student.name, student.grade_math)
-    
+
     def rank_matter_2(self):
         sorted_students = sorted(self.students, key=lambda s: s.grade_english, reverse=True)
         for student in sorted_students:
@@ -82,13 +105,9 @@ class SchoolClass:
 
 
 school_class = SchoolClass()
-school_class.add_student(Student('J', 10, 12, 13))
-school_class.add_student(Student('A', 8, 2, 17))
-school_class.add_student(Student('V', 9, 14, 14))
-
-school_class.rank_matter_1()
-school_class.rank_matter_2()
-school_class.rank_matter_3()
+school_class.add_student(StudentWithHistory(Student('J', 10, 12, 13), 15))
+school_class.add_student(StudentWithHistory(Student('A', 8, 2, 17), 11))
+school_class.add_student(StudentWithHistory(Student('V', 9, 14, 14), 7))
 
 for student in school_class:
     print(student.name, student.grade_math)
@@ -98,3 +117,6 @@ for student in StudentIterator2(school_class.students):
 
 for student in StudentIterator3(school_class.students):
     print(student.name, student.grade_science)
+
+for student in StudentIterator4(school_class.students):
+    print(student.name, student.grade_history)

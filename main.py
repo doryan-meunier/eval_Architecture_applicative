e9142cfd0f781556c3abe166e684780766a978fa
doryan-meunier <doryan.meunier@gmail.com>
2026-03-25 12:35:34 +0100

    test python

--- main.py
+++ main.py
@@ -10,6 +10,22 @@ class Grade:
 
 
 class Student:
-    def __init__(self, name, grades):
+    def __init__(self, name, grade_math, grade_english, grade_science):
         self.name = name
-        self.grades = grades
+        self.grade_math = grade_math
+        self.grade_english = grade_english
+        self.grade_science = grade_science
+
+
+class SchoolClass:
+    def __init__(self):
+        self.students = []
+
+    def add_student(self, student):
+        self.students.append(student)
+
+
+school_class = SchoolClass()
+school_class.add_student(Student('J', 10, 12, 13))
+school_class.add_student(Student('A', 8, 2, 17))
+school_class.add_student(Student('V', 9, 14, 14))
